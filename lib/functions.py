0088b987bd2592ded0e219241ef6a242528238da
sl4983 <sl4983@columbia.edu>
2022-09-21 14:52:45 -0400

    Update files

---
+++ lib/functions.py
@@ -0,0 +1,13 @@
+import gensim
+from gensim.utils import simple_preprocess
+import spacy
+
+def remove_stopwords(texts, stop_words):
+    return [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts]
+
+def lemmatization(texts, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV'], nlp=None):
+    texts_out = []
+    for sent in texts:
+        doc = nlp(" ".join(sent))
+        texts_out.append([token.lemma_ for token in doc if token.pos_ in allowed_postags])
+    return texts_out
